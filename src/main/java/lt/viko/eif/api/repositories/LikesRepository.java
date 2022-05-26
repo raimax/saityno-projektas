59d22904f1ac77b7b7eff48fbc9dd7c62eccdfb2
raimax <4miner44@gmail.com>
2022-05-26 15:34:05 +0300

    pridėtos duomenų repozitorijos

---
+++ src/main/java/lt/viko/eif/api/repositories/LikesRepository.java
@@ -0,0 +1,7 @@
+package lt.viko.eif.api.repositories;
+
+import lt.viko.eif.api.models.Like;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LikesRepository extends CrudRepository<Like, Integer> {
+}
---
+++ src/main/java/lt/viko/eif/api/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package lt.viko.eif.api.repositories;
+
+import lt.viko.eif.api.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
