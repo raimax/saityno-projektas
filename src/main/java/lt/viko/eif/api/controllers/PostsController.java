3859b8630f0feaf7c7f4ba3268594615c1f03d0e
raimax <4miner44@gmail.com>
2022-05-27 00:08:46 +0300

    pridėtas maperis, pridėtas response tekstas

--- src/main/java/lt/viko/eif/api/controllers/PostsController.java
+++ src/main/java/lt/viko/eif/api/controllers/PostsController.java
@@ -1,24 +1,29 @@
 package lt.viko.eif.api.controllers;
 
 
+import lt.viko.eif.api.dtos.PostDto;
+import lt.viko.eif.api.mapstruct.MapStructMapper;
+import lt.viko.eif.api.mapstruct.MapStructMapperImpl;
 import lt.viko.eif.api.models.Post;
 import lt.viko.eif.api.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Validated
 public class PostsController {
 
     @Autowired
     private PostRepository postRepository;
+
+    private MapStructMapper mapper = new MapStructMapperImpl();
 
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
@@ -27,8 +32,12 @@ public class PostsController {
     }
 
     @PostMapping
-    public ResponseEntity<Post> addPost(Post postDto) {
-        Post post = postRepository.save(postDto);
-        return new ResponseEntity<>(post, HttpStatus.OK);
+    public ResponseEntity<String> addPost(@RequestBody @Valid PostDto postDto) {
+        Post post = mapper.map(postDto);
+        /**
+         * TOTO: save image on server, then add image name to post.image
+         */
+        //postRepository.save(post);
+        return new ResponseEntity<>("Post added successfully", HttpStatus.OK);
     }
 }
