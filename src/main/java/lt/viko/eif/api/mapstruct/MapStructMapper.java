49b600f08b5f096495381f8b4d3f804fa98d6280
raimax <4miner44@gmail.com>
2022-05-27 00:04:02 +0300

    pridėtas mapingas PostDto -> Post

--- src/main/java/lt/viko/eif/api/mapstruct/MapStructMapper.java
+++ src/main/java/lt/viko/eif/api/mapstruct/MapStructMapper.java
@@ -1,8 +1,12 @@
 package lt.viko.eif.api.mapstruct;
 
+import lt.viko.eif.api.dtos.PostDto;
+import lt.viko.eif.api.models.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MapStructMapper {
-
+    @Mapping(target = "image", ignore = true)
+    Post map(PostDto postDto);
 }
