496ed771af3b6de5ca233fa17877a46c70f22ac5
raimax <4miner44@gmail.com>
2022-05-28 01:06:30 +0300

    Pridėtas nuotraukų išsaugojimas į serverį, postai išsaugomi į db su random nuotraukos pavadinimu

--- src/main/java/lt/viko/eif/api/controllers/PostsController.java
+++ src/main/java/lt/viko/eif/api/controllers/PostsController.java
@@ -2,6 +2,7 @@ package lt.viko.eif.api.controllers;
 
 
 import lt.viko.eif.api.dtos.PostDto;
+import lt.viko.eif.api.iservice.IFileService;
 import lt.viko.eif.api.mapstruct.MapStructMapper;
 import lt.viko.eif.api.mapstruct.MapStructMapperImpl;
 import lt.viko.eif.api.models.Post;
@@ -13,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,8 @@ public class PostsController {
 
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private IFileService fileService;
 
     private MapStructMapper mapper = new MapStructMapperImpl();
 
@@ -32,12 +36,19 @@ public class PostsController {
     }
 
     @PostMapping
-    public ResponseEntity<String> addPost(@RequestBody @Valid PostDto postDto) {
+    public ResponseEntity<String> addPost(@Valid @ModelAttribute PostDto postDto) {
         Post post = mapper.map(postDto);
-        /**
-         * TOTO: save image on server, then add image name to post.image
-         */
-        //postRepository.save(post);
-        return new ResponseEntity<>("Post added successfully", HttpStatus.OK);
+
+        try {
+            String imageName = fileService.SaveFile("images/unoptimized", postDto.getImageFile());
+            post.setImage(imageName);
+            post.getUser().setId(postDto.getUserId());
+            postRepository.save(post);
+
+            return new ResponseEntity<>("Post added successfully", HttpStatus.OK);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>("Error adding post", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
