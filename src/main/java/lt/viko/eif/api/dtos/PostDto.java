083d7f8b77044f40c595a7608363dd82eec5b77c
raimax <4miner44@gmail.com>
2022-05-28 01:04:59 +0300

    pridėtas failų tipas, vietoje user objekto padarytas userId integer, nes form data frontende nepalaiko objektų

--- src/main/java/lt/viko/eif/api/dtos/PostDto.java
+++ src/main/java/lt/viko/eif/api/dtos/PostDto.java
@@ -2,18 +2,20 @@ package lt.viko.eif.api.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
-import lt.viko.eif.api.models.User;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class PostDto {
 
     @NotNull(message = "Title is required")
+    @Size(min = 1, max = 100, message = "Title should be between 1 and 100 characters long")
     private String title;
     @NotNull(message = "Image is required")
-    private String image;
-    @NotNull(message = "User is required")
-    private User user;
+    private MultipartFile imageFile;
+    @NotNull(message = "User id is required")
+    private Integer userId;
 }
