d52c5da1abed645d6614ad41054b104a01e31dbd
raimax <4miner44@gmail.com>
2022-05-29 22:16:58 +0300

    Pridėjus naują postą, jis išsaugomas į azure storage, o ne į projekto aplanką.

--- src/main/java/lt/viko/eif/api/controllers/PostsController.java
+++ src/main/java/lt/viko/eif/api/controllers/PostsController.java
@@ -2,11 +2,11 @@ package lt.viko.eif.api.controllers;
 
 
 import lt.viko.eif.api.dtos.PostDto;
-import lt.viko.eif.api.iservice.IFileService;
 import lt.viko.eif.api.mapstruct.MapStructMapper;
 import lt.viko.eif.api.mapstruct.MapStructMapperImpl;
 import lt.viko.eif.api.models.Post;
 import lt.viko.eif.api.repositories.PostRepository;
+import lt.viko.eif.api.service.AzureStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +26,7 @@ public class PostsController {
     @Autowired
     private PostRepository postRepository;
     @Autowired
-    private IFileService fileService;
+    private AzureStorageService storageService;
 
     private MapStructMapper mapper = new MapStructMapperImpl();
 
@@ -52,12 +52,12 @@ public class PostsController {
         Post post = mapper.map(postDto);
 
         try {
-            String imageName = fileService.SaveFile("images/unoptimized", postDto.getImageFile());
+            String imageName = storageService.uploadFile(postDto.getImageFile());
             post.setImage(imageName);
             post.getUser().setId(postDto.getUserId());
             postRepository.save(post);
 
-            return new ResponseEntity<>(imageName, HttpStatus.OK);
+            return new ResponseEntity<>("Post added successfully", HttpStatus.OK);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return new ResponseEntity<>("Error adding post", HttpStatus.INTERNAL_SERVER_ERROR);
