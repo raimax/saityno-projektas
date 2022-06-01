f2c75e4f2400508360b2b198d50c76265041dc65
raimax <4miner44@gmail.com>
2022-06-01 15:10:36 +0300

    Atnaujinti endpointai, kad naudotų posto servisą

--- src/main/java/lt/viko/eif/api/controllers/PostsController.java
+++ src/main/java/lt/viko/eif/api/controllers/PostsController.java
@@ -3,61 +3,48 @@ package lt.viko.eif.api.controllers;
 
 import lt.viko.eif.api.dtos.PostDto;
 import lt.viko.eif.api.mapstruct.MapStructMapper;
-import lt.viko.eif.api.mapstruct.MapStructMapperImpl;
 import lt.viko.eif.api.models.Post;
-import lt.viko.eif.api.repositories.PostRepository;
-import lt.viko.eif.api.service.AzureStorageService;
+import lt.viko.eif.api.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.webjars.NotFoundException;
 
 import javax.validation.Valid;
-import java.io.IOException;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/posts")
 @Validated
 public class PostsController {
+    private final PostService postService;
 
     @Autowired
-    private PostRepository postRepository;
-    @Autowired
-    private AzureStorageService storageService;
-
-    private MapStructMapper mapper = new MapStructMapperImpl();
+    public PostsController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
-        List<Post> posts = (List<Post>) postRepository.findAll();
-        return new ResponseEntity<>(posts, HttpStatus.OK);
+        return new ResponseEntity<>(postService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<?> getPost(@PathVariable Integer id) {
         try {
-            Post post = postRepository.findById(id).orElseThrow();
+            Post post = postService.getById(id);
             return new ResponseEntity<>(post, HttpStatus.OK);
         }
-        catch (NoSuchElementException e) {
+        catch (Exception e) {
             return new ResponseEntity<>("Post not found", HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
     public ResponseEntity<String> addPost(@Valid @ModelAttribute PostDto postDto) {
-        Post post = mapper.map(postDto);
-
         try {
-            String imageName = storageService.uploadFile(postDto.getImageFile());
-            post.setImage(imageName);
-            post.getUser().setId(postDto.getUserId());
-            postRepository.save(post);
-
+            postService.add(postDto);
             return new ResponseEntity<>("Post added successfully", HttpStatus.OK);
         } catch (Exception e) {
             System.out.println(e.getMessage());
