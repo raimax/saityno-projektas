0ac024487da6498640a280d9049eabe498fe37f3
raimax <4miner44@gmail.com>
2022-06-01 19:36:00 +0300

    Įgalintas caching

--- src/main/java/lt/viko/eif/api/ApiApplication.java
+++ src/main/java/lt/viko/eif/api/ApiApplication.java
@@ -2,7 +2,9 @@ package lt.viko.eif.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ApiApplication {
 
