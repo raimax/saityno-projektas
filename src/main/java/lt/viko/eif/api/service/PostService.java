49c0e70c662f23e1ef5ef5d976e6b75f12e6a3e2
raimax <4miner44@gmail.com>
2022-06-01 15:10:44 +0300

    Initial

---
+++ src/main/java/lt/viko/eif/api/service/PostService.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.api.service;
+
+import lt.viko.eif.api.dtos.PostDto;
+import lt.viko.eif.api.models.Post;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface PostService {
+    Post getById(Integer id);
+    List<Post> getAll();
+    void add(PostDto post) throws IOException;
+}
