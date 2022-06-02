673cdcb6dfc9c901e35b3dc4331ee2a8f7417d81
raimax <4miner44@gmail.com>
2022-06-02 11:09:59 +0300

    pataisyta klaida

--- src/main/java/lt/viko/eif/api/models/Post.java
+++ src/main/java/lt/viko/eif/api/models/Post.java
@@ -22,7 +22,7 @@ public class Post {
     public Post() {
     }
     
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user = new User();
 
