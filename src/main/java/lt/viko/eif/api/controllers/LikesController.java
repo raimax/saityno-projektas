79ccd6c53fe0a3939f3a59600bf7578882d9b110
raimax <4miner44@gmail.com>
2022-06-02 11:12:03 +0300

    pridėtas like servisas, padaryta, jei postas jau palaikintas, laikas yra pašalinamas

--- src/main/java/lt/viko/eif/api/controllers/LikesController.java
+++ src/main/java/lt/viko/eif/api/controllers/LikesController.java
@@ -1,30 +1,34 @@
 package lt.viko.eif.api.controllers;
 
 import lt.viko.eif.api.dtos.LikeDto;
-import lt.viko.eif.api.mapstruct.MapStructMapper;
-import lt.viko.eif.api.mapstruct.MapStructMapperImpl;
-import lt.viko.eif.api.models.Like;
-import lt.viko.eif.api.repositories.LikesRepository;
+import lt.viko.eif.api.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/likes")
+@Validated
 public class LikesController {
 
     @Autowired
-    private LikesRepository likesRepository;
+    private LikeService likeService;
 
     @PostMapping
-    public ResponseEntity<String> addLike(LikeDto likeDto) {
-        Like like = new Like();
-        like.getUser().setId(likeDto.getUserId());
-        like.getPost().setId(likeDto.getPostId());
-        likesRepository.save(like);
-        return new ResponseEntity<>("Post liked successfully", HttpStatus.CREATED);
+    public ResponseEntity<String> addLike(@RequestBody @Valid LikeDto likeDto) {
+        if (!likeService.userLikedAlready(likeDto.getUserId(), likeDto.getPostId())) {
+            likeService.add(likeDto);
+            return new ResponseEntity<>("Post liked successfully", HttpStatus.OK);
+        }
+
+        likeService.remove(likeDto);
+        return new ResponseEntity<>("Post unliked", HttpStatus.OK);
     }
 }
