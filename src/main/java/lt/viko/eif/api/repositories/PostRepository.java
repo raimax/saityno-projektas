ca8cb800bd0b0b39b93674ecfc6e563582990721
raimax <4miner44@gmail.com>
2022-06-03 15:55:49 +0300

    pridėtas metodas kuris gauna atsitinktinį postą

--- src/main/java/lt/viko/eif/api/repositories/PostRepository.java
+++ src/main/java/lt/viko/eif/api/repositories/PostRepository.java
@@ -1,7 +1,10 @@
 package lt.viko.eif.api.repositories;
 
 import lt.viko.eif.api.models.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    @Query(value = "SELECT * FROM post ORDER BY rand() LIMIT 1", nativeQuery = true)
+    Post getRandom();
 }
