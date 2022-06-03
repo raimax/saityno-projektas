d0d95270e196774beed45dc410ccfbc62be4ad66
raimax <4miner44@gmail.com>
2022-06-03 16:38:12 +0300

    Pašalintas resursų vedlys, nes nebenaudojamas. Front-end nuorodos įdėtos į final string.

---
+++ src/main/java/lt/viko/eif/api/config/WebConfig.java
@@ -0,0 +1,17 @@
+package lt.viko.eif.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final String developmentOrigin = "http://localhost:3000";
+    private final String productionOrigin = "https://saityno-projektas-front-end.vercel.app";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(developmentOrigin, productionOrigin);
+    }
+}
