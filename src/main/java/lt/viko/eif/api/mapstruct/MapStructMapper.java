297e610ff5552e779d69a319ea55d693430c2c00
raimax <4miner44@gmail.com>
2022-06-03 13:39:05 +0300

    pridėtas komentaro mapping

--- src/main/java/lt/viko/eif/api/mapstruct/MapStructMapper.java
+++ src/main/java/lt/viko/eif/api/mapstruct/MapStructMapper.java
@@ -1,6 +1,8 @@
 package lt.viko.eif.api.mapstruct;
 
+import lt.viko.eif.api.dtos.CommentDto;
 import lt.viko.eif.api.dtos.PostDto;
+import lt.viko.eif.api.models.Comment;
 import lt.viko.eif.api.models.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,4 +14,5 @@ public interface MapStructMapper {
     @Mapping(target = "image", ignore = true)
     @Mapping(target = "user", ignore = true)
     Post map(PostDto postDto);
+    Comment map(CommentDto commentDto);
 }
