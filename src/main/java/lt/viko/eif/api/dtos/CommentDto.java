4b870d318c60e8dcc18c2fa455e2c41cc5a98650
raimax <4miner44@gmail.com>
2022-06-03 13:36:48 +0300

    Initial

---
+++ src/main/java/lt/viko/eif/api/dtos/CommentDto.java
@@ -0,0 +1,24 @@
+package lt.viko.eif.api.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lt.viko.eif.api.models.Post;
+import lt.viko.eif.api.models.User;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class CommentDto {
+    @JsonProperty("content")
+    private String content;
+
+    @NotNull(message = "user is not provided")
+    @JsonProperty("user")
+    private User user;
+
+    @NotNull(message = "post is not provided")
+    @JsonProperty("post")
+    private Post post;
+}
