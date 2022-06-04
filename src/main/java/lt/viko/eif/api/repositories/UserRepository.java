f9037263acb97d3221d74cae34ff10fa87ef41de
Edvinas <edvinas.juodsnukis@stud.viko.lt>
2022-06-04 23:01:46 +0300

    pridėtas metodas tikrinantis ar vartotojas egzistuoja

--- src/main/java/lt/viko/eif/api/repositories/UserRepository.java
+++ src/main/java/lt/viko/eif/api/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package lt.viko.eif.api.repositories;
 import lt.viko.eif.api.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
 }
