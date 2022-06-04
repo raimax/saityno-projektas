d13751b5a26c8029e6040294bc6e07ce57881d26
Edvinas <edvinas.juodsnukis@stud.viko.lt>
2022-06-04 23:01:14 +0300

    pridėtos rolės

--- src/main/java/lt/viko/eif/api/models/User.java
+++ src/main/java/lt/viko/eif/api/models/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,4 +21,10 @@ public class User {
     @JsonIgnore
     @Column(name = "password_hash")
     private String passwordHash;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 }
