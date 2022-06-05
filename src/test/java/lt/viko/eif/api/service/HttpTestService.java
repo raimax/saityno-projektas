776039093805b9352dade830fce4f996b9356005
raimax <4miner44@gmail.com>
2022-06-05 20:23:41 +0300

    pakeistas json maperis iš gson i jackson

--- src/test/java/lt/viko/eif/api/service/HttpTestService.java
+++ src/test/java/lt/viko/eif/api/service/HttpTestService.java
@@ -1,5 +1,7 @@
 package lt.viko.eif.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.google.gson.Gson;
 import com.squareup.okhttp.*;
 
@@ -22,15 +24,14 @@ public class HttpTestService implements HttpService<Response> {
 
     @Override
     public Response post(String url, Object o) {
-        Gson g = new Gson();
-        String likeDtoJSON =  g.toJson(o);
-
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        RequestBody body = RequestBody.create(JSON, likeDtoJSON);
+        ObjectMapper mapper = JsonMapper.builder().findAndAddModules().build();
 
         try {
-            Request request = new Request.Builder().url(url).post(body).build();
+            String data = mapper.writeValueAsString(o);
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(JSON, data);
 
+            Request request = new Request.Builder().url(url).post(body).build();
             return client.newCall(request).execute();
         } catch (IOException e) {
             System.out.println("Unsuccessful endpoint call " + e.getMessage());
