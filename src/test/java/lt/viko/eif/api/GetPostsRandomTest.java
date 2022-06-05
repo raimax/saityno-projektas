20112bad451b2e0a1a1ec4f291c6075ef7c79348
raimax <4miner44@gmail.com>
2022-06-05 21:46:35 +0300

    pridėtos validacijos, pakeisti scenarijai

--- src/test/java/lt/viko/eif/api/GetPostsRandomTest.java
+++ src/test/java/lt/viko/eif/api/GetPostsRandomTest.java
@@ -10,20 +10,23 @@ import org.junit.Assert;
 import java.io.IOException;
 
 public class GetPostsRandomTest extends AppTest {
-    @When("The user sends GET request to the random posts endpoint")
-    public void theUserSendsGetRequestRandomPostsEndpoint() {
+    @When("The user sends GET request to the random post endpoint")
+    public void theUserSendsGetRequestToRandomPostEndpoint() {
         response = httpTestService.get(API_URL + "/posts/random");
     }
 
-    @Then("The HTTP status of random posts is OK")
-    public void theHTTPStatusOfRandomPostsIsOk() {
+    @Then("The HTTP status of random post is OK")
+    public void theHTTPStatusOfRandomPostIsOk() {
         Assert.assertEquals(200, response.code());
     }
 
-    @Then("The Response contains a list of random posts")
-    public void theResponseContainsAListOfRandomPosts() throws IOException {
+    @Then("The Response contains a random post")
+    public void theResponseContainsRandomPost() throws IOException {
         ObjectMapper mapper = JsonMapper.builder().findAndAddModules().build();
         Post post = mapper.readValue(response.body().string(), Post.class);
 
+        Assert.assertTrue(post.getTitle() != null);
+        Assert.assertTrue(post.getId() != null);
+        Assert.assertTrue(post.getImage() != null);
     }
 }
